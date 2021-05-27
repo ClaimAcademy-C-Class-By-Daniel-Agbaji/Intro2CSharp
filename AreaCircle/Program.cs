a2203dce74baa2fd8380851e3b2e9bb447543ac9
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 11:42:43 -1000

    Add project files.

---
+++ AreaCircle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DanWayMondayAssignment
+{
+    class AreaCircProgram
+    {
+        static void Main(string[] args)
+        {
+            double PI = 3.142;
+            double nRadius, nArea, nCircumference;
+
+            Console.Write("Please enter the radius of your circle: ");
+            nRadius = double.Parse(Console.ReadLine());
+
+            nArea = PI * (nRadius * nRadius);
+
+            Console.WriteLine("The area of your circle is " + nArea);
+
+            nCircumference = (PI * nRadius) * 2;
+            Console.WriteLine("The circumference of your circle is " + nCircumference);
+
+            Console.ReadLine();
+        }
+    }
+}
