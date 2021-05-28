33ebc843938119d906e4d89b46c039ba40a0517e
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:47:26 -1000

    Add files via upload

---
+++ RoundingNumbers/RoundingNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number with a decimal: ");
+
+            double nDouble = double.Parse(Console.ReadLine());
+
+            int intNumber = (int) Math.Round(nDouble);
+
+            Console.WriteLine("You entered " + nDouble + ", the new value is " + intNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
