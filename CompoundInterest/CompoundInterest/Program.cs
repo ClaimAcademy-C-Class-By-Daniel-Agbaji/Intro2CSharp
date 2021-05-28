775dc957375338b5114f942e34a5212d3a2fc84e
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:48:49 -1000

    Add files via upload

---
+++ CompoundInterest/CompoundInterest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CompoundInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nInvestment;
+            double nInterest;
+            int nYears;
+            double nCompoundInterest = 0;
+
+            Console.Write("Please enter the investment amount: ");
+            nInvestment = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter the Interest Rate: ");
+            nInterest = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            Console.Write("Please enter number of Years: ");
+            nYears = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= nYears; i++)
+            {
+                nCompoundInterest = nInvestment * Math.Pow((1 + nInterest), nYears);
+            }
+
+            {
+                Console.WriteLine("The compound interest is: $", nCompoundInterest);
+            }
+            Console.ReadLine();
+        }
+    }
+}
