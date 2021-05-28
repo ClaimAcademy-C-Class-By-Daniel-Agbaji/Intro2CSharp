5cb403af86a445a47d39545ddc9951bc2202e072
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:46:34 -1000

    Add files via upload

---
+++ AreaTriangle/AreaTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nBaseWidth, nArea, nHeight;
+
+            Console.Write("Please enter the base-width of your triangle: ");
+            nBaseWidth = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the height of your triangle :");
+            nHeight = double.Parse(Console.ReadLine());
+
+            nArea = (nBaseWidth * nHeight) / 2;
+
+            Console.WriteLine("The area of yor triangle is " + nArea);
+
+            Console.Read();
+        }
+    }
+}
