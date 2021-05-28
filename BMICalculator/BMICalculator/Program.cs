f6749ce0cab356d055acf8ac6bde967cfd300689
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:48:05 -1000

    Add files via upload

---
+++ BMICalculator/BMICalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your weight in pounds: ");
+            decimal lb = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Please enter your height in inches: ");
+            decimal ft = Convert.ToDecimal(Console.ReadLine());
+
+            decimal nBMI = lb / (ft * ft) * 703;
+            Console.WriteLine("BMI is " + Math.Round(nBMI, 4));
+
+            Console.ReadLine();
+        }
+    }
+}
