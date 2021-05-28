919a73cd1410c6ef7622a0baf49f26c543f96b6a
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:51:22 -1000

    Add files via upload

---
+++ Ascii/Ascii/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Ascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a letter of the alphabet: ");
+            string strLetter = Console.ReadLine();
+            byte[] asciiValue = Encoding.ASCII.GetBytes(strLetter);
+
+            foreach (var value in asciiValue)
+            {
+                Console.WriteLine("The ASCII value of " + strLetter + " is " + value);
+            }
+            Console.Read();
+        }
+    }
+}
