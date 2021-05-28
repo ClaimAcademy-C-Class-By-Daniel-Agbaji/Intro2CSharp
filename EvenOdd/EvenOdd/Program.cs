b58e20de13c01bd6bc04fcf3a4599a8979ade8c3
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:50:32 -1000

    Add files via upload

---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+
+            int intNumber = Console.Read();
+
+            if (intNumber % 2 == 0)
+                Console.WriteLine("RESULT: Even");
+            else
+                Console.WriteLine("RESULT: Odd");
+
+                Console.Read();
+        }
+    }
+}
