8e1dcbb2c5c24cee38512fb2b1eaa869eb7d07c1
DANIEL AGBAJI <agbajidaniel@hotmail.com>
2021-06-05 13:59:11 -0500

    Initial commit

---
+++ Week1/Methods/Methods/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Methods
+{
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        // Formular for an area of a circle is 2pir2 = value of my area of a circle. 
+    }
+}
