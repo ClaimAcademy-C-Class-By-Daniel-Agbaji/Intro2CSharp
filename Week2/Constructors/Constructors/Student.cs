e5869613bf40a86afc534cee080a45bd3a293c4e
DANIEL AGBAJI <agbajidaniel@hotmail.com>
2021-06-13 21:35:03 -0500

    Added

--- Week2/Constructors/Constructors/Student.cs
+++ Week2/Constructors/Constructors/Student.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace Constructors
 {
-    class Student
+    public class Student
     {
+        private readonly string _firstName;
 
+        private readonly string _lastName;
+
+        private readonly int _iD;
+        public Student(int iD, string lastName, string firstName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _iD = iD;
+        }
+
+        public void GetStudentDetails(Student student)
+        {
+            
+
+        }
     }
 }
