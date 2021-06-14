43225ab7863b2b384b5ef0d820bc6fef3725683a
DANIEL AGBAJI <agbajidaniel@hotmail.com>
2021-06-14 18:21:51 -0500

    code changed

---
+++ CollectionAndGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionAndGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
