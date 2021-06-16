c7056b525c67751b2b7eaf210baa29dd621c8cba
DANIEL AGBAJI <agbajidaniel@hotmail.com>
2021-06-15 20:16:32 -0500

    initial commit

---
+++ Week2/ObjectClass/DemoEntityFrameWork/Entity/UserDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoEntityFrameWork.Entity
+{
+    public class UserDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=User;"
+                + "Trusted_Connection=True;"));
+        }
+    }
+}
